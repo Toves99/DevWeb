b4cd2b71b9cad4a464eb85cbf6f1f07ddf3cbaf8
Toves99 <114761179+Toves99@users.noreply.github.com>
2023-08-05 10:25:24 +0300

    first update

---
+++ src/main/java/backendCode/Controller/UserController.java
@@ -0,0 +1,12 @@
+package backendCode.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/hello")
+    public String getHelloMessage(){
+        return "Hello world";
+    }
+}
